589d0ab910324298db26bce2781050169e59e93c
trungntn <trungntn@yes4all.com>
2024-06-12 15:40:22 +0700

    a

---
+++ test_dag_2.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+
+default_args = {
+    "retries": 20,
+    "retry_delay": timedelta(minutes=4),
+    "start_date": datetime(2023, 7, 31, 0, 0, 0),
+    "owner": 'j_ai_biec',
+    "email_on_retry": False,
+    "email_on_failure": True,
+}
+dag = DAG(
+    dag_id='6',
+    description='test DAG',
+    schedule_interval=None,
+    max_active_runs=1,
+    concurrency=1,
+    default_args=default_args,
+    catchup=False,
+)
+
+with dag:
+    @task
+    def main():
+        print('hello cl')
+
+    @task
+    def main2():
+        print('hello cl too')
+
+    @task
+    def main3():
+        print('hello cl too too')
+    
+    main() >> main2() >> main3()
