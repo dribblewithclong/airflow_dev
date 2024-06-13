3569ffa0fb3edc96bebbe5c5b2d1645ea84d1b52
nguyen cao long <longnc@yes4all.com>
2024-06-13 14:11:58 +0700

    test profile

--- test_dag.py
+++ test_dag.py
@@ -35,13 +35,60 @@ with dag:
 
         print(result1.stdout)
         print(result2.stdout)
+    
+    @task
+    def main15():
+        import os
+        import yaml
+
+        def create_and_append_yaml(file_path, new_content):
+            # Check if the file exists
+            if not os.path.exists(file_path):
+                # Create an empty file if it doesn't exist
+                with open(file_path, 'w') as file:
+                    yaml.dump({}, file)  # Dump an empty dictionary to initialize the file
+            
+            # Read the existing content of the YAML file
+            with open(file_path, 'r') as file:
+                existing_content = yaml.safe_load(file) or {}
+            
+            # Update the existing content with new content
+            if isinstance(existing_content, dict):
+                existing_content.update(new_content)
+            else:
+                raise TypeError("Existing content is not a dictionary")
+
+            # Write the updated content back to the file
+            with open(file_path, 'w') as file:
+                yaml.dump(existing_content, file)
+        
+        create_and_append_yaml(
+            file_path='~/.dbt/profiles.yml',
+            new_content={
+                'bi_proc': {
+                    'outputs': {
+                        'dev': {
+                            'dbname': 'y4a_datamart',
+                            'host': '172.30.105.111',
+                            'pass': 'VNAs!asdlkj1230sa',
+                            'port': 5432,
+                            'schema': 'y4a_cdm',
+                            'threads': 1,
+                            'type': 'postgres',
+                            'user': 'y4a_dd_trungntn'
+                        }
+                    },
+                    'target': 'dev'
+                }
+            }
+        )
 
     @task.bash
     def main2():
         return """
         mkdir -p /opt/airflow/dbt_jobs/bi_proc &&
         cd /opt/airflow/dbt_jobs/bi_proc &&
-        whoami
+        dbt debug
         """
 
     @task.bash
