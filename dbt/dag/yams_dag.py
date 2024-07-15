18ab48dce778b6bff6e3cdc52b2d35dca3f5c101
Nguyen Tran Nam Trung <trungntn@yes4all.internal>
2024-07-15 16:16:43 +0700

    a

---
+++ dbt/dag/yams_dag.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+import sys, os 
+sys.path.append(os.path.dirname(__file__))
+
+default_args = {
+    "retries": 20,
+    "retry_delay": timedelta(minutes=4),
+    "start_date": datetime(2023, 7, 31, 0, 0, 0),
+    "owner": 'j_ai_biec',
+    "email_on_retry": False,
+    "email_on_failure": True,
+}
+dag = DAG(
+    dag_id='yams_dbt',
+    description='test DAG',
+    schedule_interval="0 6 * * *",
+    max_active_runs=1,
+    concurrency=1,
+    default_args=default_args,
+    catchup=False,
+)
+
+with dag:    
+    yams_task = BashOperator(
+        task_id='yams_task',
+        bash_command="""
+        mkdir -p /opt/airflow/dbt_jobs &&
+        cp -f -r /opt/airflow/dags/airflow_iykyk/dbt/bi_proc /opt/airflow/dbt_jobs/ &&
+        cd /opt/airflow/dbt_jobs/bi_proc &&
+        dbt run --select tag:yams
+        rm -r /opt/airflow/dbt_jobs/bi_proc
+        """,
+    )
+
+    yams_task
