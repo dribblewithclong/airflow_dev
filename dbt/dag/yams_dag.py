d80517643852229723555bb15a2654ced090bcc5
Nguyen Tran Nam Trung <trungntn@yes4all.internal>
2024-07-15 16:17:30 +0700

    a

--- dbt/dag/yams_dag.py
+++ dbt/dag/yams_dag.py
@@ -12,7 +12,7 @@ default_args = {
     "retries": 20,
     "retry_delay": timedelta(minutes=4),
     "start_date": datetime(2023, 7, 31, 0, 0, 0),
-    "owner": 'j_ai_biec',
+    "owner": 'trungntn',
     "email_on_retry": False,
     "email_on_failure": True,
 }
